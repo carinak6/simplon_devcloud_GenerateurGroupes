e5c20973f3b1c91d1b2c95b7d4d275ea447bd096
carinak6 <kassiscarina@gmail.com>
2021-02-13 15:49:01 +0100

    petite modification d'affichage 2

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,6 +7,14 @@ let listeEleves = [];/*liste des eleves, je l'utilise pour ajouter a chaque fois
 let groupes=[]; /*liste des groupes, que j'afficherai a la fin de la creation des groupes*/
 
 
+if(localStorage.getItem('eleves')){
+    let elevesLocal = localStorage.getItem('eleves');
+    array_eleves = elevesLocal.split(',');
+    for(eleve of array_eleves){
+        document.querySelector("#listeEleves").innerHTML += "<li>"+eleve+"</li>";
+    }
+}
+
 
 /* Ajoute les noms dans la liste Eleves*/
 document.querySelector('#envoyer').addEventListener('click', function(){
@@ -18,8 +26,7 @@ document.querySelector('#envoyer').addEventListener('click', function(){
     
     
     localStorage.setItem('eleves',listeEleves);//enregistre dans le localstorage la liste eleves
-   // console.log(localStorage.getItem('eleves'));
-
+   
     document.querySelector('.name').value="";//reinitialise le champ nom
 });
 
@@ -29,67 +36,64 @@ document.querySelector('#envoyer').addEventListener('click', function(){
 document.querySelector("#creation").addEventListener('click', function(){
     /*il faudra factoriser le code avec des functions*/
 
-    /*****utilise le local storage *****/
+    
     let nb= parseInt(document.querySelector("#tailleGroupe").value); /** je reçois la taille du groupe */
 
     let eleves = localStorage.getItem('eleves');// il return en un type string
     arrayEleves = eleves.split(','); //je cree un tableaux des noms
+
+    /*copie de l'array original */
+    copieArrayEleves = [...arrayEleves];
     console.log(arrayEleves);
     
     let taille = arrayEleves.length;
         
-    /** mais je pourrai utiliser listeEleves */
-    //il y a listeEleves comme variable avec le noms je peut l utiliser
-
     let sousGroupes=[];
     groupes.length=0;
 
     if(taille > 0){// pour savoir si il y a des eleves dans le local storage
-                
-        while(taille >0){
+
+        //je fait un loop que creera un groupe pour chaque tour        
+        while(taille > 0 ){
         
-            //generation des groupes
+            //generation des groupes d'une taille nb
             for(let i=0; i<nb; i++ ){ 
+
                 /*console.log(i);*/
-                
+
                 //on choisie al hazar le nro d element
-                let choix = Math.floor(Math.random() * taille);  
-                
+                let choix = Math.floor(Math.random() * taille);                          
                 
                 //on cree le subgroupe
-                sousGroupes.push(arrayEleves[choix]);
+                sousGroupes.push(copieArrayEleves[choix]);
 
                 //j enleve le element de la liste des eleves                
-                arrayEleves.splice(choix, 1)
+                copieArrayEleves.splice(choix, 1);               
                 
-                taille -=1;//pour controler la sortie
-
-                if(i== arrayEleves.length){
-                    break;
-                }
-
-                
+                taille --;//pour controler la sortie
                
             }
-            groupes.push(sousGroupes);
             
+            //on enregistre dans la liste des groupes
+            groupes.push(sousGroupes);   
             sousGroupes=[];
+
+            //on verifie pour pas faire un autre loop avec les derniers eleves qui restent dans la liste qui sont la meme taille ou plus ppt que la taille demandé
+            if(nb >= (copieArrayEleves.length)){
+                groupes.push(copieArrayEleves);
+                taille = 0; //pour sortir de la boucle                
+            }           
 
         }
         
-        console.log(groupes);        
+        //console.log(groupes);        
         document.querySelector("div#groupes").textContent="";
 
         for(let x =0; x < groupes.length; x++){
          
-            document.querySelector("div#groupes").innerHTML += "<h3>Groupe : "+(x+1) +"</h3> ";            
-
-
-            document.querySelector("div#groupes").innerHTML += groupes[x].join(' - ');
-                       
-            document.querySelector("div#groupes").innerHTML +="<br>";
-
-            localStorage.setItem('groupe'+x,groupes[x].join(' - ')); //je l utiliser apres
+            document.querySelector("div#groupes").innerHTML += "<h3>Groupe : "+(x+1) +"</h3> ";
+            document.querySelector("div#groupes").innerHTML += groupes[x].join(' - ');                       
+            document.querySelector("div#groupes").innerHTML +="<br>";            
 
         }
         
